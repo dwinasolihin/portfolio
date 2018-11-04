60590e491deaa1992774f4f94e26676b32345f06
Dwina Solihin <dwinasolihin@Dwinas-Work-MacBook-Pro.local>
2018-11-04 15:30:25 -0800

    removing folders

--- .cache/api-runner-browser-plugins.js
+++
@@ -1,7 +0,0 @@
-module.exports = [{
-      plugin: require('/Users/dwinasolihin/Desktop/portfolio/node_modules/gatsby-plugin-offline/gatsby-browser'),
-      options: {"plugins":[]},
-    },{
-      plugin: require('/Users/dwinasolihin/Desktop/portfolio/gatsby-browser.js'),
-      options: {"plugins":[]},
-    }]
