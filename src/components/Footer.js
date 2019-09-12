d3ace7297df6358f8c29a183ecba6a1fb447b19f
Dwina <dwinasolihin@gmail.com>
2019-09-12 13:28:51 -0700

    changing email

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,7 +6,7 @@ class Footer extends React.Component {
             <section id="footer">
                 <ul className="copyright">
                 <ul className="icons">
-                    <li><a href="mailto:dwina@uw.edu" className="icon alt fa-envelope"><span className="label">Gatsby.JS</span></a></li>
+                    <li><a href="mailto:dwinasolihin@gmail.com" className="icon alt fa-envelope"><span className="label">Gatsby.JS</span></a></li>
                     <li><a href="https://www.linkedin.com/in/dwinasolihin/" className="icon alt fa-linkedin"><span className="label">Sass</span></a></li>
                     <li><a href="https://github.com/dwinasolihin/" className="icon alt fa-github"><span className="label">React.JS</span></a></li>
                 </ul>
