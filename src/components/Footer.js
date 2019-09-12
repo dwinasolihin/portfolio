bc945fd2ac41f07b66ed696f14e3705f909be913
Dwina <dwinasolihin@gmail.com>
2019-09-12 13:42:12 -0700

    Fixing typo

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -10,7 +10,7 @@ class Footer extends React.Component {
                     <li><a href="https://www.linkedin.com/in/dwinasolihin/" className="icon alt fa-linkedin"><span className="label">Sass</span></a></li>
                     <li><a href="https://github.com/dwinasolihin/" className="icon alt fa-github"><span className="label">React.JS</span></a></li>
                 </ul>
-                    <li>&copy; 2019, Create by Dwina Solihin</li>
+                    <li>&copy; 2019, Created by Dwina Solihin</li>
                     <li><span id="siteseal"><script async type="text/javascript" src="https://seal.godaddy.com/getSeal?sealID=vgqh6GxqRlbBWqUP9Dh5GvzGgNDen2IZZh55sKjX58SOj50avDiZK7cMuTt0"></script></span></li>
                 </ul>
             </section>
